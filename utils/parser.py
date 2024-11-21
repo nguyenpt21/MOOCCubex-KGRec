38d58e046e11fe6d4a0bd0afd96bdc41fea407ec
nguyenpt21 <22520983@gm.uit.edu.vn>
2024-11-21 10:44:07 +0700

    first commit

--- utils/parser.py
+++ utils/parser.py
@@ -5,8 +5,8 @@ def parse_args_kgsr():
     parser = argparse.ArgumentParser(description="MKG")
     # ===== log ===== #
     parser.add_argument('--desc', type=str, default="", help='EXP description')
-    parser.add_argument('--log', action='store_true', default='log', help='log in file or not')
-    parser.add_argument('--log_fn', type=str, default=None, help='log file_name')
+    parser.add_argument('--log', action='store_true', default=True, help='log in file or not')
+    parser.add_argument('--log_fn', type=str, default='log', help='log file_name')
     # ===== dataset ===== #
     parser.add_argument("--dataset", nargs="?", default="last-fm", help="Choose a dataset:[last-fm,amazon-book,alibaba]")
     parser.add_argument(
